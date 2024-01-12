23b6b81f25f79f86659ee99c83abe07909fab6a1
yasmineleschi <yasmineleschi01@gmail.com>
2024-01-12 12:51:15 +0100

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutsModule } from './layouts/layouts.module';
 import { FormsModule } from '@angular/forms';
-import { ViewsComponent } from './views/views/views.component';
 import { ProfileRoutingModule } from './views/front/profile/profile-routing.module';
+import { NavbarComponent } from './views/navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewsComponent,
-
-
+    NavbarComponent,
 
   ],
   imports: [
